56f30e607cb5e0770b8f358a4ec4ca95c84ade2c
GmanLoL <168227581+GmanLoL@users.noreply.github.com>
2026-02-09 21:16:44 +0300

    'AAA'

---
+++ Cafeteriawebsitedesign/vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react(),tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+    host: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  preview: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
